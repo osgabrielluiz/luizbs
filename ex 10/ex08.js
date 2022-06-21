7b6741c3b7d73a1b302c0bf777bde3969118fbce
osgabrielluiz <91032465+osgabrielluiz@users.noreply.github.com>
2022-06-21 20:13:27 -0300

    js

---
+++ ex 10/ex08.js
@@ -0,0 +1,11 @@
+let velocidade = 80
+
+console.log(`Sua velocidade é de ${velocidade} Km/h`)
+
+if(velocidade > 60){
+    console.log(`Você foi multado!`)
+}
+else if(velocidade < 60){
+console.log(`Continue sua rota...`)
+}
+console.log(`Use o cinto de segurança`)
